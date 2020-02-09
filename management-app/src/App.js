080aaf0e589bb248d2f03640251f1dedccea90bf
Eric-Zhang-Projects <zhang.eric39@gmail.com>
2020-02-09 17:12:28 -0500

    first commit

---
+++ management-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import LoginComponent from './components/LoginComponent.jsx';
+
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <LoginComponent></LoginComponent>
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ management-app/src/components/LoginComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LoginComponent extends Component {
+    render(){
+      return (
+        <div>Login!!</div>
+      );
+    }
+  }
+  
+  export default LoginComponent;
