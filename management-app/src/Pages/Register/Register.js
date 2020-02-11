e699f9c19fd90d786728b5747ce488d840aaa6ae
Eric-Zhang-Projects <zhang.eric39@gmail.com>
2020-02-10 22:04:51 -0500

    register page

--- management-app/src/Pages/Register/Register.js
+++ management-app/src/Pages/Register/Register.js
@@ -1,7 +1,69 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import './Register.css';
 
-export default class Register extends Component {
-  render() {
-    return <div>hi</div>;
+export default function Register(props) {
+
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [RUID, setRUID] = useState("");
+  const [Email, setEmail] = useState("");
+
+  function validateForm() {
+    return FirstName.length > 0 && LastName.length > 0 && RUID.length > 0 && Email.length > 0;
   }
-}
+
+  async function handleSubmit(event) {
+    if (!validateForm()) {
+      alert("Login Failed");
+    } else {
+      console.log(FirstName, LastName, RUID, Email);
+      props.history.push("/");
+    }
+  }
+
+    return (
+      <div className="Register">
+      <h1 className="title">Register:</h1>
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="FirstName" bsSize="large">
+          <FormLabel>First Name</FormLabel>
+          <FormControl
+            autoFocus
+            type="FirstName"
+            value={FirstName}
+            onChange={e => setFirstName(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="LastName" bsSize="large">
+          <FormLabel>Last Name</FormLabel>
+          <FormControl
+            value={LastName}
+            type="LastName"
+            onChange={e => setLastName(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="RUID" bsSize="large">
+          <FormLabel>RUID</FormLabel>
+          <FormControl
+            autoFocus
+            type="RUID"
+            value={RUID}
+            onChange={e => setRUID(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="Email" bsSize="large">
+          <FormLabel>Email</FormLabel>
+          <FormControl
+            value={Email}
+            type="Email"
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Register
+        </Button>
+      </form>
+    </div>
+    );
+  }
